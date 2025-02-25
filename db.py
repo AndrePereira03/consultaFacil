c26718c211305b3c40252938946a6e4638dccc48
AndrePereira03 <andre.pereira@estudante.ufjf.br>
2025-02-24 21:27:20 -0300

    cleaning the code

--- db.py
+++ db.py
@@ -5,7 +5,7 @@ db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
-    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"  # Pode trocar por MySQL/PostgreSQL
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     
     db.init_app(app)
